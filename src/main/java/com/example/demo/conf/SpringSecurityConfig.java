96d5565f0e1fc8f4ee279a48d86da946c6e346fd
shijia <1623299667@qq.com>
2018-07-23 10:47:56 +0800

    内存中配置用户

--- src/main/java/com/example/demo/conf/SpringSecurityConfig.java
+++ src/main/java/com/example/demo/conf/SpringSecurityConfig.java
@@ -1,10 +1,12 @@
 package com.example.demo.conf;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -26,5 +28,13 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 				.and()
 			.csrf()//关闭默认的csrf认证
 				.disable();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()//设置一个内存中的用户密码，早期的版本不需要设置密码登陆，springsecurity5之后需要
+				.passwordEncoder(new BCryptPasswordEncoder()).withUser("admin")
+				.password(new BCryptPasswordEncoder().encode("123456")).roles("ADMIN");
 	}
 }
